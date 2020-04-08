26ca1d42387e0f49d8f9c431d6a0410faced1461
David Carlisle <d.p.carlisle@gmail.com>
2020-04-08 13:30:31 +0100

    escape quotes

--- scripts/runlatex.js
+++ scripts/runlatex.js
@@ -16,7 +16,7 @@ function llexamples() {
 	o.setAttribute("onclick",'openinoverleaf("pre' + i + '")');
 	p[i].parentNode.insertBefore(o, p[i].nextSibling);
 	var f=document.createElement("span");
-	f.innerHTML="<form style=\"display:none\" id=\"form-pre" + i +"\" action=\"https://www.overleaf.com/docs" method="post">
+	f.innerHTML="<form style=\"display:none\" id=\"form-pre" + i +"\" action=\"https://www.overleaf.com/docs\" method=\"post\">
        <input id=\"encoded_snip-pre" + i + "\" name=\"encoded_snip\" value=\"\" /></form>";
 	p[i].parentNode.insertBefore(f, p[i].nextSibling);
     }
