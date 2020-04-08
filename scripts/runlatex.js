5607109d782ebf10dcabe68774f99f1955b835c3
David Carlisle <d.p.carlisle@gmail.com>
2020-04-08 14:49:40 +0100

    copy to clipboard

--- scripts/runlatex.js
+++ scripts/runlatex.js
@@ -3,14 +3,22 @@ function llexamples() {
     for(var i=0;i<p.length;i++) {
     if(p[i].textContent.indexOf("\\documentclass") !== -1) {
 	p[i].setAttribute("id","pre" + i);
+	// edit
+	var b = document.createElement("button");
+	b.innerText="edit";
+	b.setAttribute("onclick",'allowedit("pre' + i + '")');
+	p[i].parentNode.insertBefore(b, p[i]);
+	// copy
+	var c = document.createElement("button");
+	c.innerText="copy";
+	c.setAttribute("onclick",'copytoclipboard("pre' + i + '")');
+	p[i].parentNode.insertBefore(c, p[i]);
+	// latexonline
 	var r = document.createElement("button");
 	r.innerText="latexonline.cc";
 	r.setAttribute("onclick",'latexonlinecc("pre' + i + '")');
 	p[i].parentNode.insertBefore(r, p[i].nextSibling);
-	var b = document.createElement("button");
-	b.innerText="edit";
-	b.setAttribute("onclick","pre" + i + '.contentEditable="true"');
-	p[i].parentNode.insertBefore(b, p[i].nextSibling);
+	// overleaf
 	var o = document.createElement("button");
 	o.innerText="Open in OverLeaf";
 	o.setAttribute("onclick",'openinoverleaf("pre' + i + '")');
@@ -41,6 +49,24 @@ function openinoverleaf(nd) {
   var p = document.getElementById(nd);
   document.getElementById('encoded_snip-' + nd ).value =encodeURIComponent(p.innerText);
   document.getElementById('form-' + nd).submit();
+}
+
+
+function copytoclipboard(nd){
+    var p = document.getElementById(nd);
+    var nn=document.createElement("textarea");
+    nn.value=p.innerText;
+    document.body.appendChild(nn);
+    nn.select();
+    document.execCommand("copy");
+    document.body.removeChild(nn);
+}
+
+
+function allowedit(nd){
+    var p = document.getElementById(nd);
+    p.contentEditable="true";
+    p.style.border="solid thin green";
 }
 
 
