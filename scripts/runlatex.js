ae32bda3dbe30a11cd92935c1df2e9b4bf8cb24b
David Carlisle <d.p.carlisle@gmail.com>
2020-07-02 22:53:13 +0100

    switch to latexcgi server

--- scripts/runlatex.js
+++ scripts/runlatex.js
@@ -29,7 +29,7 @@ function llexamples() {
 	    // latexonline
 	    var r = document.createElement("button");
 	    r.innerText=buttons["LaTeX Online"];
-	    r.setAttribute("onclick",'latexonlinecc("pre' + i + '")');
+	    r.setAttribute("onclick",'latexcgi("pre' + i + '")');
 	    r.setAttribute("id","lo-pre" + i);
 	    p[i].parentNode.insertBefore(r, p[i].nextSibling);
 	    // overleaf
@@ -43,9 +43,7 @@ function llexamples() {
 	    f.innerHTML="<form style=\"display:none\" id=\"form-pre" + i +"\" action=\"https://www.overleaf.com/docs\" method=\"post\" target=\"_blank\"></form>";
 	    p[i].parentNode.insertBefore(f, p[i].nextSibling);
 	    var f2=document.createElement("span");
-	    // action=\"https://httpbin.org/post\"
-	    // action=\"https://latex.ytotech.com/builds/sync\"
-	    f2.innerHTML="<form style=\"display:none\" id=\"form2-pre" + i + "\" name=\"form2-pre" + i +"\" enctype=\"multipart/form-data\" action=\"https://latex.ytotech.com/builds/sync\" method=\"post\" target=\"pre" + i + "ifr\"></form>";
+	    f2.innerHTML="<form style=\"display:none\" id=\"form2-pre" + i + "\" name=\"form2-pre" + i +"\" enctype=\"multipart/form-data\" action=\"https://latexcgi.xyz/cgi-bin/p2\" method=\"post\" target=\"pre" + i + "ifr\"></form>";
 	    p[i].parentNode.insertBefore(f2, p[i].nextSibling);
 	}
     }
@@ -53,63 +51,6 @@ function llexamples() {
 
 const commentregex = / %.*/;
 const engineregex = /% *!TEX.*[^a-zA-Z]((pdf|xe|lua|u?p)latex)/i;
-
-function latexonlinecc(nd) {
-    var p = document.getElementById(nd);
-    var t = p.innerText;
-    var cmd="";
-    var eng=t.match(engineregex);
-    if(eng != null) {
-	cmd="&command=" + eng[1].toLowerCase();
-    } else if(t.indexOf("fontspec") !== -1) {
-	cmd="&command=xelatex";
-    }
-    // no platex on latexonlinecc
-    // checking User Agent isn't great but better than looping
-    if(window.navigator.userAgent.match(/Android.*Firefox/) ||
-       cmd == "&command=platex" ||
-       cmd == "&command=uplatex" ){ 
-	latexonhttp(nd);
-    } else {
-	var fconts="";
-	if(typeof(preincludes) == "object") {
-	    if(typeof(preincludes[nd]) == "object") {
-		fconts= "\n\\makeatletter\\def\\input@path{{latex.out/}}\\makeatother\n";
-		var incl=preincludes[nd];
-		for(prop in incl) {
-		    fconts=fconts+"\n\\begin{filecontents}{" +
-			incl[prop] +
-			"}\n" +
-			document.getElementById(prop).innerText +
-			"\n\\end{filecontents}\n";
-		}
-	    }
-	}
-	// no biber support currently
-	if(t.indexOf("biblatex") !== -1) {
-	    t=t.replace(/usepackage\{biblatex/,'usepackage[]\{biblatex');
-	    t=t.replace(/\]\{biblatex/,',backend=bibtex]\{biblatex');
-	}
-	var b = document.getElementById('lo-' + nd);
-	var ifr= document.getElementById(nd + "ifr");
-	if(ifr == null) {
-	    ifr=document.createElement("iframe");
-	    ifr.setAttribute("width","100%");
-	    ifr.setAttribute("height","500em");
-	    ifr.setAttribute("id",nd + "ifr");
-	    ifr.setAttribute("name",nd + "ifr");
-	    p.parentNode.insertBefore(ifr, b.nextSibling);
-	    d=document.createElement("button");
-	    d.innerText=buttons["Delete Output"];
-	    d.setAttribute("id","del-" + nd);
-	    d.setAttribute("onclick",'deleteoutput("' + nd + '")');
-	    p.parentNode.insertBefore(d, b.nextSibling);
-	}
-	// that looks to have all lines but still need to zap comments for some reason..
-	ifr.setAttribute("src","https://texlive2020.latexonline.cc/compile?text=" + encodeURIComponent(fconts.replace(commentregex,'') + t.replace(engineregex,'')) + cmd);
-    }
-}
-
 
 
 // https://www.overleaf.com/devs
@@ -126,6 +67,14 @@ function addinputnoenc(f,n,v) {
     inp.setAttribute("type","text");
     inp.setAttribute("name",n);
     inp.value =v;
+    f.appendChild(inp);
+}
+
+function addtextarea(f,n,v) {
+    var inp=document.createElement("textarea");
+    inp.setAttribute("type","text");
+    inp.setAttribute("name",n);
+    inp.textContent=v;
     f.appendChild(inp);
 }
 
@@ -162,62 +111,6 @@ function openinoverleaf(nd) {
     fm.submit();
 }
 
-// https://github.com/YtoTech/latex-on-http
-
-// This could share more code with latexonline version
-function latexonhttp(nd) {
-    var jsn=[];
-    if(typeof(preincludes) == "object") {
-	if(typeof(preincludes[nd]) == "object") {
-	    var incl=preincludes[nd];
-	    for(prop in incl) {
-		jsn.push({path: incl[prop],
-			  content: document.getElementById(prop).innerText
-			 })
-	    }
-	}
-    }
-    var p = document.getElementById(nd);
-    var t = p.innerText;
-    // no biber support currently
-    if(t.indexOf("biblatex") !== -1) {
-	t=t.replace(/usepackage\{biblatex/,'usepackage[]\{biblatex');
-	t=t.replace(/\]\{biblatex/,',backend=bibtex]\{biblatex');
-    }
-    jsn.push({main: true, content: t});
-    var b = document.getElementById('lo-' + nd);
-    var ifr= document.getElementById(nd + "ifr");
-    if(ifr == null) {
-	ifr=document.createElement("iframe");
-	ifr.setAttribute("width","100%");
-	ifr.setAttribute("height","500em");
-	ifr.setAttribute("id",nd + "ifr");
-	ifr.setAttribute("name",nd + "ifr");
-	p.parentNode.insertBefore(ifr, b.nextSibling);
-	d=document.createElement("button");
-	d.innerText=buttons["Delete Output"];
-	d.setAttribute("id","del-" + nd);
-	d.setAttribute("onclick",'deleteoutput("' + nd + '")');
-	p.parentNode.insertBefore(d, b.nextSibling);
-    }
-    var cmd="pdflatex";
-    var eng=t.match(engineregex);
-    if(eng != null) {
-	cmd=eng[1].toLowerCase();
-    } else if(t.indexOf("fontspec") !== -1) {
-	cmd="xelatex";
-    }
-    var fm = document.getElementById('form2-' + nd);
-    fm.innerHTML="";
-    addinputnoenc(fm,"compiler",cmd);
-    addinputnoenc(fm,"resources",JSON.stringify(jsn));
-    fm.submit();
-}
-
-
-
-//
-
 
 function copytoclipboard(nd){
     var p = document.getElementById(nd);
@@ -245,5 +138,53 @@ function deleteoutput(nd){
     ifr.parentNode.removeChild(ifr);
 }
 
+function latexcgi(nd) {
+    var fm = document.getElementById('form2-' + nd);
+    fm.innerHTML="";
+    var p = document.getElementById(nd);
+    var t = p.innerText;
+    addtextarea(fm,"filecontents[]",t);
+    addinputnoenc(fm,"filename[]","document.tex");
+    if(typeof(preincludes) == "object") {
+	if(typeof(preincludes[nd]) == "object") {
+	    var incl=preincludes[nd];
+	    for(prop in incl) {
+		addtextarea(fm,"filecontents[]",document.getElementById(prop).innerText);
+		addinputnoenc(fm,"filename[]",incl[prop]);
+	    }
+	}
+    }
+    var engv="pdflatex";
+    var eng=t.match(engineregex);
+    if(eng != null) {
+	engv=eng[1].toLowerCase();
+    } else if(t.indexOf("fontspec") !== -1) {
+	engv="xelatex";
+    }
+    addinput(fm,"engine",engv);
+    	var b = document.getElementById('lo-' + nd);
+	var ifr= document.getElementById(nd + "ifr");
+	if(ifr == null) {
+	    ifr=document.createElement("iframe");
+	    ifr.setAttribute("width","100%");
+	    ifr.setAttribute("height","500em");
+	    ifr.setAttribute("id",nd + "ifr");
+	    ifr.setAttribute("name",nd + "ifr");
+	    p.parentNode.insertBefore(ifr, b.nextSibling);
+	    d=document.createElement("button");
+	    d.innerText=buttons["Delete Output"];
+	    d.setAttribute("id","del-" + nd);
+	    d.setAttribute("onclick",'deleteoutput("' + nd + '")');
+	    p.parentNode.insertBefore(d, b.nextSibling);
+	}
+    var  loading=document.createElement("div");
+    loading.id=nd+"load";
+    loading.textContent="Loading . . .";
+    p.parentNode.insertBefore(loading, ifr);
+    setTimeout(function () {
+	p.parentNode.removeChild(document.getElementById(nd+"load"));
+    }, 1000);
+    fm.submit();
+}
 
 window.addEventListener('load', llexamples, false);
