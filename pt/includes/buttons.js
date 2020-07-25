157a030f247743e15f9cfb1a3a5acd2a00a94604
PhelypeOleinik <tex.phelype@gmail.com>
2020-07-25 01:18:57 -0300

    Add portuguese translation of buttons.js

--- pt/includes/buttons.js
+++ pt/includes/buttons.js
@@ -1,8 +1,8 @@
 var buttons ={
     "edit":             "editar",
     "copy":             "copiar",
-    "Open in Overleaf": "Abrir em Overleaf",
+    "Open in Overleaf": "Abrir no Overleaf",
     "LaTeX Online":     "LaTeX Online",
     "Delete Output":    "Excluir saída",
-    "Compiling PDF":    "Compiling PDF"
+    "Compiling PDF":    "Construindo PDF"
 }
