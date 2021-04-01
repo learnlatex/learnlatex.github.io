cbf3fcfb47ea546a3a4c272adbeaa55b7971d996
David Carlisle <d.p.carlisle@gmail.com>
2021-04-01 20:18:55 +0100

    theme

--- assets/scripts/runlatex.js
+++ assets/scripts/runlatex.js
@@ -396,7 +396,7 @@ var rldefaultengine=getCookie('runlatex-engine');
 if(rldefaultengine=="") rldefaultengine="pdflatex";
 
 
-var rlacetheme=getCookie('runlatex-acethemenum');
+var rlacethemenum=getCookie('runlatex-acethemenum');
 if(rlacethemenum=="") rlacethemenum="9";
 var rlacetheme=getCookie('runlatex-acetheme');
 if(rlacetheme=="") rlacetheme="ace/theme/textmate";
