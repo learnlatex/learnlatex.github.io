fbd813483bcd914682e4b105688a0902aae2a5d8
David Carlisle <d.p.carlisle@gmail.com>
2021-04-01 17:31:20 +0100

    cookies

--- assets/scripts/runlatex.js
+++ assets/scripts/runlatex.js
@@ -345,4 +345,53 @@ function latexcgi(nd) {
     fm.submit();
 }
 
+
+
+
+
+var createCookie = function(name, value, days) {
+    var expires;
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toGMTString();
+    }
+    else {
+        expires = "";
+    }
+    document.cookie = name + "=" + value + expires + "; path=/" + "; SameSite=Lax";
+}
+
+function getCookie(c_name) {
+    if (document.cookie.length > 0) {
+        c_start = document.cookie.indexOf(c_name + "=");
+        if (c_start != -1) {
+            c_start = c_start + c_name.length + 1;
+            c_end = document.cookie.indexOf(";", c_start);
+            if (c_end == -1) {
+                c_end = document.cookie.length;
+            }
+            return unescape(document.cookie.substring(c_start, c_end));
+        }
+    }
+    return "";
+}
+
+function rlSetReturn(n) {
+    createCookie('runlatex-return',n,100);
+}
+
+var rldefaultreturn=getCookie('runlatex-return');
+if(rldefaultreturn=="") rldefaultreturn="pdfjs";
+
+function rlSetEngine(n) {
+    createCookie('runlatex-engine',n,100);
+}
+
+var rldefaultengine=getCookie('runlatex-engine');
+if(rldefaultengine=="") enginez="pdflatex";
+
+
+
+
 window.addEventListener('load', llexamples, false);
