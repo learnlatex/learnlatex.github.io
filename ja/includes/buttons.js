0f8bcaf7d427dc915bf81354149f88575b79a255
Takuto ASAKURA <wtsnjp@gmail.com>
2021-04-27 02:03:56 +0900

    set platex as default engine for ja

--- ja/includes/buttons.js
+++ ja/includes/buttons.js
@@ -1,8 +1,8 @@
-runlatex.texts ={
+runlatex.texts = {
     "Open in Overleaf": "Overleaf で開く",
-    "TeXLive.net":  "LaTeX Online",
+    "TeXLive.net":  "",
     "Delete Output": "出力を削除",
-    "Compiling PDF":    "Compiling PDF",
+    "Compiling PDF": "PDF をコンパイル",
     // The following not used on learnlatex.org
     "edit": "編集",
     "copy": "コピー",
@@ -10,3 +10,6 @@ runlatex.texts ={
     "End Added Code":   "End Added code",
     "Top Caption":      "Edit and run this example:"
 }
+
+var rldefaultengine=getCookie('runlatex-engine');
+if(rldefaultengine=="") rldefaultengine="platex";
