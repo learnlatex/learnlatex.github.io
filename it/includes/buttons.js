3a209f8ad1f43f1b816919c4bb4e542fca6f1676
David Carlisle <d.p.carlisle@gmail.com>
2022-11-01 14:25:57 +0000

    add

---
+++ it/includes/buttons.js
@@ -0,0 +1,13 @@
+runlatex.texts ={
+    "Open in Overleaf": "(it) Open in Overleaf",
+    "TeXLive.net":      "(it) Run at TeXLive.net",
+    "Delete Output":    "(it) Delete Output",
+    "Compiling PDF":    "(it) Compiling PDF",
+    // The following not used on learnlatex.org
+    "edit":             "(it) edit",
+    "copy":             "(it) copy",
+    "Added Code":       "(it) Added code",
+    "End Added Code":   "(it) End Added code",
+    "Top Caption":      "(it) Edit and run this example:"
+}
+
