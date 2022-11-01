56c900b4419ab895f1c927f9541a0f43902b9315
Gordon-72 <66129349+Gordon-72@users.noreply.github.com>
2022-11-01 21:17:38 +0100

    Update buttons.js

--- it/includes/buttons.js
+++ it/includes/buttons.js
@@ -2,7 +2,7 @@ runlatex.texts ={
     "Open in Overleaf": "(it) Apri in Overleaf",
     "TeXLive.net":      "(it) Esegui in TeXLive.net",
     "Delete Output":    "(it) Elimina l'output",
-    "Compiling PDF":    "(it) Componi il PDF",
+    "Compiling PDF":    "(it) Compongo il PDF",
     // The following not used on learnlatex.org
     "edit":             "(it) modifica",
     "copy":             "(it) copia",
