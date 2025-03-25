26f324c5141061c482840e8042c76f841365784e
Nikos Platis <nplatis@gmail.com>
2025-03-25 14:39:28 +0200

    Initial setup for Greek translation

---
+++ el/includes/buttons.js
@@ -0,0 +1,13 @@
+runlatex.texts ={
+    "Open in Overleaf": "Άνοιγμα στο Overleaf",
+    "TeXLive.net":      "Τρέξιμο στο TeXLive.net",
+    "Delete Output":    "Διαγραφή αποτελέσματος",
+    "Compiling PDF":    "Δημιουργία PDF",
+    // The following not used on learnlatex.org
+    "edit":             "επεξεργασία",
+    "copy":             "αντιγραφή",
+    "Added Code":       "Νέος κώδικας",
+    "End Added Code":   "Τέλος νέου κώδικα",
+    "Top Caption":      "Επεξεργασία και τρέξιμο αυτού του παραδείγματος:"
+}
+
